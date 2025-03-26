1f22ba62d0596cbdf4608f8306ef88dd49445ec2
jessisanabria20@gmail.com <jessisanabria20@gmail.com>
2025-03-26 15:12:56 -0500

    Subiendo el proyecto

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  // Los modulos son funciones anadidas de nuxt
+  modules: ["@nuxt/ui"],
+  // Importacion global del archivo de estilos (nuxt ui y tailwind)
+  css: ["~/assets/css/main.css"],
+  // Para importar automaticamnete los componentes en las vistas
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+});
---
+++ pages/servicios/web.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Desarrollamos webs</h1>
+  </div>
+</template>
